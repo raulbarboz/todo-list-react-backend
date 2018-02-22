61ec6e9e3a3cf1606e9e0a3a274ebd042684c13f
raulbarboz <ruzito@gmail.com>
2018-02-22 11:46:02 -0300

    heroku setup

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const databaseURL = process.env.MONGODB_URI;
 
-module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect(databaseURL)
