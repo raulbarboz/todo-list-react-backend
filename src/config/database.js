c84dcf904254126f0189456a63cde9ba4959b1e0
ruzito <ruzito@gmail.com>
2018-02-22 21:29:31 -0300

    fixed CORS erros

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const databaseURL = process.env.MONGODB_URI;
+const databaseURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/todo';
 
 module.exports = mongoose.connect(databaseURL, { useMongoClient: true })
